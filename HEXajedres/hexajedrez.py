c60cf996077ecac8b62321cc65463c4b561deece
ezequielMaureira <ezequiel.maureira@alumnos.frm.utn.edu.ar>
2023-05-16 20:56:34 -0300

    cree el menu de pausa

--- HEXajedres/hexajedrez.py
+++ HEXajedres/hexajedrez.py
@@ -8,7 +8,7 @@ pygame.init()
 ANCHO_PANTALLA = 1100
 ALTO_PANTALLA = 700
 
-pantalla = pygame.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
+pantalla = pygame.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA),pygame.RESIZABLE)
 
 
 # Titulo de la pantall
@@ -24,6 +24,22 @@ COLOR_LETRAS = (255, 255, 255)
 
 # variables menu
 opcionesDeJuego = False
+pantallaDePausa = False
+
+# Funciones
+def dibujaTexto(texto, fuente, colorTexto, x, y):
+    img = fuente.render(texto, True, colorTexto)
+    pantalla.blit(img, (x, y))
+    
+def cargar_imagen(rutaImagen):
+    imagen = pygame.image.load(rutaImagen)
+    anchoImagen = ANCHO_PANTALLA
+    relacionAspecto = imagen.get_width() / imagen.get_height()
+    altoImagen = int(anchoImagen / relacionAspecto)
+    x = 0
+    y = int(ALTO_PANTALLA / 2 - altoImagen / 2)
+    imagenEscalada = pygame.transform.scale(imagen, (anchoImagen, altoImagen))
+    return imagenEscalada
 
 # Imagenes del Menu
 unoContraUnoImg = pygame.image.load("img/boton_uno_contra_uno.png").convert_alpha()
@@ -37,33 +53,37 @@ unoContraDosCpuImg = pygame.image.load(
 hexajedrezImg = pygame.image.load("img/HEXajedrez.png").convert_alpha()
 
 # Imagen de fondo
-fondoRojoImg = pygame.transform.scale(
-    pygame.image.load("img/Back_Red.png").convert_alpha(),
-    (ANCHO_PANTALLA, ALTO_PANTALLA),
-)
-fondoAmarilloImg = pygame.transform.scale(
-    pygame.image.load("img/Back_Amarillo.png").convert_alpha(),
-    (ANCHO_PANTALLA, ALTO_PANTALLA),
-)
+fondoRojoImg = cargar_imagen("img/Back_Red.png")
+fondoAmarilloImg = cargar_imagen("img/Back_Amarillo.png")
 
 # Crear Botones
 botonUnoContraUno = Boton(304, 245, unoContraUnoImg, 1)
 botonUnoContraDos = Boton(304, 345, unoContraDosImg, 1)
 botonUnoContraCpu = Boton(304, 445, unoContraCpuImg, 1)
 botonUnoContraDosCpu = Boton(304, 545, unoContraDosCpuImg, 1)
-
-
-# Funciones
-def dibujaTexto(texto, fuente, colorTexto, x, y):
-    img = fuente.render(texto, True, colorTexto)
-    pantalla.blit(img, (x, y))
-
+botonContinuar = Boton(304,245,pygame.font.SysFont("arialblack", 40).render("Continuar", True, (255,255,255)),1)
+botonOpciones = Boton(304,295,pygame.font.SysFont("arialblack", 40).render("Opciones", True, (255,255,255)),1)
+botonComoJugar = Boton(304,345,pygame.font.SysFont("arialblack", 40).render("Como jugar", True, (255,255,255)),1)
+botonCreditos = Boton(304,395,pygame.font.SysFont("arialblack", 40).render("Creditos", True, (255,255,255)),1)
+botonSalir = Boton(304,445,pygame.font.SysFont("arialblack", 40).render("Salir", True, (255,255,255)),1)
 
 # bucle del juego
 ejecucion = True
 while ejecucion:
     pantalla.fill((52, 78, 91))
-    if opcionesDeJuego == True:
+    if pantallaDePausa == True:
+        pantalla.fill((50,50,50,128))
+        if botonContinuar.dibujar(pantalla):
+            print("Continuar")
+        if botonOpciones.dibujar(pantalla):
+            print("Opciones")
+        if botonComoJugar.dibujar(pantalla):
+            print("Como Jugar") 
+        if botonCreditos.dibujar(pantalla):
+            print("Creditos") 
+        if botonSalir.dibujar(pantalla):
+            print("Salir")  
+    elif opcionesDeJuego == True:
         pantalla.blit(fondoAmarilloImg, (0, 0))
         pantalla.blit(hexajedrezImg, (100, 20))
         if botonUnoContraUno.dibujar(pantalla):
@@ -81,14 +101,21 @@ while ejecucion:
 
     # Escucha eventos
     for evento in pygame.event.get():
+        
+        # si se presiona el boton de cierre se termina el bucle
+        if evento.type == pygame.QUIT:
+             ejecucion = False
+        elif evento.type == pygame.VIDEORESIZE:
+            ANCHO_PANTALLA, ALTO_PANTALLA = evento.size
+            pantalla = pygame.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA), pygame.RESIZABLE)
         # si se aprieta una tecla se valida que
-        if evento.type == pygame.KEYDOWN:
+        elif evento.type == pygame.KEYDOWN:
             # si la tecla presionada es espacio
             if evento.key == pygame.K_SPACE:
                 opcionesDeJuego = True
-        # si se presiona el boton de cierre se termina el bucle
-        if evento.type == pygame.QUIT:
-            ejecucion = False
+            if evento.key == pygame.K_ESCAPE:
+                pantallaDePausa = not pantallaDePausa
+        
     pygame.display.update()
 pygame.quit()
 # sleep(3)#Prueba para ver la ventana funcionar
