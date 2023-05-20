be77fc35b90e25480da42ce139ffaad3a947f0dd
FranciscoAlejandroVarberde <francisco.varberde@alumnos.frm.utn.edu.ar>
2023-05-20 15:32:48 -0300

    se actualizo el archivo tablero.py piezas.py

--- HEXajedrez/hexajedrez.py
+++ HEXajedrez/hexajedrez.py
@@ -22,7 +22,7 @@ icono = Imagen("img/hex_icono.png")
 pygame.display.set_icon(icono.obtenerImagen())
 
 # Se define la tipografia
-FUENTE = pygame.font.Font("fnt/8-Bit.TTF",30)
+FUENTE = pygame.font.Font("fnt/8-Bit.TTF", 30)
 # Se define el color de las letras
 BLANCO = (255, 255, 255)
 
@@ -33,11 +33,15 @@ pantallaDeOpciones = False
 pantallaAzul = False
 
 # Funciones
+
+
 def dibujaTexto(texto, tamanio, colorTexto, x, y):
     """ Genenera un texto en pantalla con la FUENTE y color de texto elegidos en la 
     posicion "x" e "y" de la pantalla con el texto elegido. """
-    img = pygame.font.Font("fnt/8-Bit.TTF",tamanio).render(texto, True, colorTexto)
+    img = pygame.font.Font(
+        "fnt/8-Bit.TTF", tamanio).render(texto, True, colorTexto)
     pantalla.blit(img, (x, y))
+
 
 # Imagenes del Menu
 unoContraUnoImg = Imagen("img/boton_uno_contra_uno.png")
@@ -61,16 +65,16 @@ botonUnoContraUno = Boton(0, 245, unoContraUnoImg.obtenerImagen(), 1)
 botonUnoContraDos = Boton(0, 345, unoContraDosImg.obtenerImagen(), 1)
 botonUnoContraCpu = Boton(0, 445, unoContraCpuImg.obtenerImagen(), 1)
 botonUnoContraDosCpu = Boton(0, 545, unoContraDosCpuImg.obtenerImagen(), 1)
-botonContinuar = Boton(0,245,FUENTE.render("Continuar", True, BLANCO),1)
-botonOpciones = Boton(0,295,FUENTE.render("Opciones", True, BLANCO),1)
-botonComoJugar = Boton(0,345,FUENTE.render("Como jugar", True, BLANCO),1)
-botonCreditos = Boton(0,395,FUENTE.render("Creditos", True, BLANCO),1)
-botonSalir = Boton(0,445,FUENTE.render("Salir", True, BLANCO),1)
-botonSonido = Boton(0,245,FUENTE.render("Sonido", True, BLANCO),1)
-botonMusica = Boton(0,295,FUENTE.render("Musica", True, BLANCO),1)
-botonDificultad = Boton(0,345,FUENTE.render("Dificultad", True, BLANCO),1)
-botonVolver = Boton(0,395,FUENTE.render("Volver", True, BLANCO),1)
-botonVolverAzul = Boton(850,600,FUENTE.render("Volver", True, BLANCO),1)
+botonContinuar = Boton(0, 245, FUENTE.render("Continuar", True, BLANCO), 1)
+botonOpciones = Boton(0, 295, FUENTE.render("Opciones", True, BLANCO), 1)
+botonComoJugar = Boton(0, 345, FUENTE.render("Como jugar", True, BLANCO), 1)
+botonCreditos = Boton(0, 395, FUENTE.render("Creditos", True, BLANCO), 1)
+botonSalir = Boton(0, 445, FUENTE.render("Salir", True, BLANCO), 1)
+botonSonido = Boton(0, 245, FUENTE.render("Sonido", True, BLANCO), 1)
+botonMusica = Boton(0, 295, FUENTE.render("Musica", True, BLANCO), 1)
+botonDificultad = Boton(0, 345, FUENTE.render("Dificultad", True, BLANCO), 1)
+botonVolver = Boton(0, 395, FUENTE.render("Volver", True, BLANCO), 1)
+botonVolverAzul = Boton(850, 600, FUENTE.render("Volver", True, BLANCO), 1)
 
 
 # Bucle de ejecucion
@@ -78,11 +82,12 @@ ejecucion = True
 while ejecucion:
     # Pintar la pantalla de color blanco
     pantalla.fill(BLANCO)
-    
+
     # PANTALLA DE PAUSA
     if pantallaDePausa == True:
         # se muestra el fondo celeste y se dibujan los botones
-        pantalla.blit(fondoCelesteImg.redimensionar(ANCHO_PANTALLA, ALTO_PANTALLA), (0 ,0))
+        pantalla.blit(fondoCelesteImg.redimensionar(
+            ANCHO_PANTALLA, ALTO_PANTALLA), (0, 0))
         # se chequea que boton se presiona y se abre la ventana correspondiente
         if botonContinuar.dibujar(pantalla, "Centrado"):
             print("Continuar")
@@ -91,17 +96,18 @@ while ejecucion:
             pantallaDeOpciones = True
             print("Opciones")
         if botonComoJugar.dibujar(pantalla, "Centrado"):
-            print("Como Jugar") 
+            print("Como Jugar")
         if botonCreditos.dibujar(pantalla, "Centrado"):
-            print("Creditos") 
+            print("Creditos")
         if botonSalir.dibujar(pantalla, "Centrado"):
             # Se abandona el bucle de ejecucion y cierra el programa
             ejecucion = False
-            print("Salir")  
+            print("Salir")
         # PANTALLA DE OPCIONES
         if pantallaDeOpciones:
             # se muestra el fondo verde con los botones de opciones
-            pantalla.blit(fondoVerdeImg.redimensionar(ANCHO_PANTALLA, ALTO_PANTALLA),(0, 0))
+            pantalla.blit(fondoVerdeImg.redimensionar(
+                ANCHO_PANTALLA, ALTO_PANTALLA), (0, 0))
             if botonSonido.dibujar(pantalla, "Centrado"):
                 print("Sonido")
             if botonMusica.dibujar(pantalla, "Centrado"):
@@ -111,21 +117,23 @@ while ejecucion:
             if botonVolver.dibujar(pantalla, "Centrado"):
                 # Se abandona la pantalla de opciones
                 pantallaDeOpciones = False
-    # PANTALLA DE MODOS DE JUEGO       
+    # PANTALLA DE MODOS DE JUEGO
     elif opcionesDeJuego == True:
         # Se selecciona el fondo mediante un condicional
         if pantallaAzul == True:
-            pantalla.blit(pantallaAzulImg.redimensionar(ANCHO_PANTALLA, ALTO_PANTALLA),(0, 0))
+            pantalla.blit(pantallaAzulImg.redimensionar(
+                ANCHO_PANTALLA, ALTO_PANTALLA), (0, 0))
             if botonVolverAzul.dibujar(pantalla, ""):
                 pantallaAzul = False
         else:
             # Se dibuja el fondo amarillo
-            pantalla.blit(fondoAmarilloImg.redimensionar(ANCHO_PANTALLA, ALTO_PANTALLA), (0, 0))
+            pantalla.blit(fondoAmarilloImg.redimensionar(
+                ANCHO_PANTALLA, ALTO_PANTALLA), (0, 0))
             # Se muestra el titulo
             pantalla.blit(hexajedrezImg.obtenerImagen(), (100, 20))
             # Se dibujan los botones de los modos de juego y al presionarlos cambia la pantalla
             if botonUnoContraUno.dibujar(pantalla, "Centrado"):
-                Juego(ANCHO_PANTALLA,"bn").iniciar()
+                Juego(ANCHO_PANTALLA, "bn").iniciar()
             if botonUnoContraDos.dibujar(pantalla, "Centrado"):
                 pantallaAzul = True
                 print("inicia juego 1 contra 2")
@@ -138,18 +146,19 @@ while ejecucion:
     # PANTALLA DE TITULO
     else:
         # se dibuja el fondo rojo
-        pantalla.blit(fondoRojoImg.redimensionar(ANCHO_PANTALLA, ALTO_PANTALLA), (0, 0))
+        pantalla.blit(fondoRojoImg.redimensionar(
+            ANCHO_PANTALLA, ALTO_PANTALLA), (0, 0))
         # se dibuja el texto en la pantalla
-        dibujaTexto("Presione ESPACIO para comenzar",30, BLANCO, 130, 500)
-        dibujaTexto("o ESCAPE para mas opciones",20, BLANCO, 295, 550)
+        dibujaTexto("Presione ESPACIO para comenzar", 30, BLANCO, 130, 500)
+        dibujaTexto("o ESCAPE para mas opciones", 20, BLANCO, 295, 550)
 
     # Escucha eventos
     for evento in pygame.event.get():
-        
+
         # si se presiona el boton de cierre se termina el bucle
         if evento.type == pygame.QUIT:
-             ejecucion = False
-             
+            ejecucion = False
+
         # actualizar la ventana al redimensionar
         elif evento.type == pygame.VIDEORESIZE:
             # Obtener las nuevas dimensiones de la pantalla
@@ -159,11 +168,12 @@ while ejecucion:
             relacion_de_aspecto = ANCHO_PANTALLA / ALTO_PANTALLA
 
             # Redimensionar la pantalla manteniendo la relacion de aspecto
-            pantalla = pygame.display.set_mode((ANCHO_PANTALLA, int(ANCHO_PANTALLA / relacion_de_aspecto)), pygame.RESIZABLE)
+            pantalla = pygame.display.set_mode((ANCHO_PANTALLA, int(
+                ANCHO_PANTALLA / relacion_de_aspecto)), pygame.RESIZABLE)
 
             # Actualizar la pantalla
             pygame.display.flip()
-            
+
         # si se aprieta una tecla se valida que
         elif evento.type == pygame.KEYDOWN:
             # si la tecla presionada es espacio
@@ -173,8 +183,8 @@ while ejecucion:
             if evento.key == pygame.K_ESCAPE:
                 pantallaDePausa = not pantallaDePausa
                 pantallaDeOpciones = False
-            
-    # Actualiza la pantalla    
+
+    # Actualiza la pantalla
     pygame.display.update()
 # Cierra el programa
 pygame.quit()
